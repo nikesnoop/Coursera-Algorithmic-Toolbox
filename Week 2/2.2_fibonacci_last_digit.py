28d7fc08f6b2789273670e48e94d194d16bba899
Yaswanth Udayakumar <68239314+nikesnoop@users.noreply.github.com>
2022-01-29 15:18:32 +0530

    Create 2.2_fibonacci_last_digit.py

---
+++ Week 2/2.2_fibonacci_last_digit.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n <= 1:
+    print(n)
+    quit()
+
+def fibo(n):
+    a, b = 0, 1
+    c = 0
+    for i in range (n-1):
+        c = a + b
+        c = c % 10
+        b, a = c, b
+    return c
+
+print(fibo(n))
